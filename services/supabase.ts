8aba3a6b0e68cc9fc1786e6cea129f95779d5600
Ilay <findusspezial@gmail.com>
2026-01-06 17:23:56 +0100

    Update supabase.ts

--- services/supabase.ts
+++ services/supabase.ts
@@ -5,7 +5,7 @@ import { createClient } from '@supabase/supabase-js';
 export const SUPABASE_URL = 'https://vhhqfubwjbwvmaijxzxz.supabase.co';
 
 // Dein Key
-export const SUPABASE_ANON_KEY = 'sb_publishable_mmM60DulSzH7YyXx04uZlw_YDlGZ'; 
+export const SUPABASE_ANON_KEY = 'sb_publishable_BUnnqBQVqvy6H6ImR9TB6w_GpnRfQiY'; 
 
 // Client-Initialisierung mit Standard-Einstellungen für Web-Apps
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY, {
